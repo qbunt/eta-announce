788d3fdd8be24d0ed9f4eaa12ea568186eb22a99
Jeremy Bunting <jeremy.bunting@spinifexgroup.com>
2017-04-17 10:38:08 -0700

    dropped icloud, reformatted a bit, added optional departure time

--- twilio.js
+++ twilio.js
@@ -2,9 +2,6 @@ require('dotenv').config()
 var client = require('twilio')(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 module.exports.notify = (to, message) => {
-    if(process.env.TWILIO_SID !== undefined && process.env.TWILIO_TOKEN !== undefined){
-        Promise.reject(console.error('missing required twilio parameters'))
-    }
     return new Promise((resolve, reject)=>{
         client.messages.create({body: message,
             to: to,
